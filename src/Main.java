a8128bc9c4156405526b6009011f467aebdeedef
a.maretskij <maretskij.a+g@gmail.com>
2023-11-09 14:01:31 +0100

    tree

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
 
         System.out.println("Hello world!");
         System.out.println("Hello world!777");
-        System.out.println("Hello world!7000git")
+        System.out.println("Hello world!7000git");
+
     }
 }
