a3b570f32f1e28477a42268da0bf0357c68440a3
a.maretskij <maretskij.a+g@gmail.com>
2023-11-11 01:38:54 +0100

    newHome

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,55 @@
+
+import java.util.*;
+
+import java.util.List;
 public class Main {
-    public static void main(String[] args) {
 
-        System.out.println("Hello world!");
-        System.out.println("Hello world!777");
-        System.out.println("Hello world!7000git");
-        System.out.println("Hello world!999");
+    public static void main(String[] args) throws Exception {
+        //Задание 1
+        /*try {
+            Car car = new Car("linsi", "Tonirovani");
+            car.setFari("linhgvgsi");
+            car.setStekla("Tonirfttovani");
+        } catch (MalformedParameterizedTypeException ex) {
+            throw new RuntimeException(ex);
+        } */
+
+        //Задание 2
+        ArrayList<Integer> cats = new ArrayList<>(10);
+
+        for (int i = 0; i < 11; i++) {
+            cats.add(i + 1);
+        }
+        System.out.println(cats);
+        cats.remove(1);
+        cats.remove(4);
+        cats.remove(6);
+        System.out.println(cats);
+
+        cats.set(2, 328);
+        System.out.println(cats);
+
+        //Задание 3
+        String[] array = new String[]{"Java", "Python", "Perl", "C++", "Java", "C++", "C#"};
+        ArrayList<String> numList = new ArrayList<>(List.of(array));
+
+        for (int i = 0; i < numList.size(); i++) {
+            System.out.println(numList.get(i));;
+        }
 
 
+
+        //Дополнительно
+
+        List<Integer> list = List.of(1, 2, 3, 5, 4, 3, 2, 1, 2, 3, 5);
+        long count = counterCatch(list, 2);
+        System.out.println("Количество чисел 2: " + count);
     }
+
+    public static long counterCatch(List<Integer> list, int number) {
+        return list.stream().filter(value -> value == number).count();
+    }
+
 }
+
+
